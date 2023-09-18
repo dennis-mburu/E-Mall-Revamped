4bbe997bfb61fd17606de9c7847dfbe86b260294
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-09-18 08:57:19 +0300

    :package: :recycle: add react-router-bootstrap and replace all a tags and hrefs

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,36 +1,45 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
       <Navbar bg="primary" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/" className="flex items-center">
-            <svg
-              className="fill-current h-8 w-8 mr-2 text-cyan-400"
-              width="54"
-              height="54"
-              color="blue"
-              viewBox="0 0 54 54"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-            </svg>
-            <h1 className="font-bold text-white">E-MALL</h1>
-          </Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand className="flex items-center">
+              <svg
+                className="fill-current h-8 w-8 mr-2 text-cyan-400"
+                width="54"
+                height="54"
+                color="blue"
+                viewBox="0 0 54 54"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
+              </svg>
+              <h1 className="font-bold text-white">E-MALL</h1>
+            </Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/cart" className="flex items-center mr-4 text-white">
-                <FaShoppingCart className="mr-1" />
-                Cart
-              </Nav.Link>
-              <Nav.Link href="/login" className="flex items-center text-white">
-                <FaUser className="mr-1" />
-                Sign In
-              </Nav.Link>
+              <LinkContainer to="cart">
+                <Nav.Link className="flex items-center mr-4 text-white">
+                  <FaShoppingCart className="mr-1" />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to="cart">
+                <Nav.Link className="flex items-center text-white">
+                  <FaUser className="mr-1" />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
