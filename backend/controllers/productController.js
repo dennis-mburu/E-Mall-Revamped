3f48c509483bd25949de27be50517b30ad2c0089
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-09-23 11:06:01 +0300

    :sparkles: add products controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,26 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+// @desc - Fetch all products
+// @route - /api/products
+// @access - Public
+const getAllProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// @desc - Fetch product by Id
+// @route - /api/products/:id
+// @access - Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not Found");
+  }
+});
+
+export { getAllProducts, getProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,29 +1,19 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import { getAllProducts, getProductById } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+// router.get(
+//   "/",
+//   // asyncHandler(async (req, res) => {
+//   //   const products = await Product.find({});
+//   //   res.json(products);
+//   // })
 
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+// );
 
-    if (product) {
-      return res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Resource not Found");
-    }
-  })
-);
+// router.route('/').get(getAllProducts)
+router.get('/', getAllProducts)
+router.get("/:id", getProductById);
 
 export default router;
