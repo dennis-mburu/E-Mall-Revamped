33722fe1c3e3f86b9e2593d4ccd117ca634b5df5
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-10-03 16:10:00 +0300

    :package: set up empty redux store and add routes constants

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,16 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+import "./index.css";
+
+import App from "./App";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 
@@ -17,8 +23,7 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
-      <Route  path="/product/:id" element={<ProductScreen />} />
-      
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
@@ -26,7 +31,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
