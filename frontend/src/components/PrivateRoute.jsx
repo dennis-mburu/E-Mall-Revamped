0780a49f15241d45caf18e1f86b21d6dc6e5620f
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-11-28 09:25:25 +0300

    :lock: add private route component to protect pages that should only be visible to logged in users

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+function PrivateRoute() {
+  const { userInfo } = useSelector((state) => state.auth);
+
+  return userInfo ? <Outlet /> : <Navigate to="/" replace={true} />;
+}
+
+export default PrivateRoute;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,6 +23,7 @@ import NewCartScreen from "./screens/NewCartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PrivateRoute from "./components/PrivateRoute";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -32,7 +33,10 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<NewCartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
