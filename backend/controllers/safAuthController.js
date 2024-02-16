712286bbd4f1c56081fbd64a8f3dbf42c75732b2
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2024-02-16 18:22:32 +0300

    :lock: move daraja's basic auth key to .env file

--- backend/controllers/safAuthController.js
+++ backend/controllers/safAuthController.js
@@ -4,16 +4,14 @@ import asyncHandler from "../middleware/asyncHandler.js";
 const url =
   "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
 
-const headers = {
-  Authorization:
-    "Basic R0p4RnJLOXhZT1RIb0lkemRWSENIbHB5WXZjQnd4cVo4VU40em9zQmJJWXplU0xVOnJ2ckJZOHRDdHdMeHhKR0cwclRqeFJnbkxZcnFoQTN5MkxaeDF5SUN3dERWS0gxWnpQVnFlejJBdzNYQnN5UzY=",
-};
 
 const safAuthenticate = asyncHandler(async (req, res) => {
   try {
     const response = await fetch(url, {
       method: "GET",
-      headers,
+      headers: {
+        Authorization: `Basic ${process.env.DARAJA_BASIC_AUTH_KEY}`,
+      },
     });
     const data = await response.text();
     res.send(data);
